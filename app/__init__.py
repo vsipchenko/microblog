b9ffba05781f38efb536eb6118eece4a5b82898b
vitaliisipchenko <vitaliisipchenko@gmail.com>
2019-02-16 14:51:28 +0200

    change logging level

--- app/__init__.py
+++ app/__init__.py
@@ -67,10 +67,10 @@ def create_app(config_class=Config):
         file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
         file_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
+        file_handler.setLevel(logging.ERROR)
         app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
+        app.logger.setLevel(logging.ERROR)
         app.logger.info('Microblog startup')
 
     return app
